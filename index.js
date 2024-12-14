ef5581da3e009d8220f50a43d23f1e3a887e9268
Mochammad khaerul ilman <110533356+khaerulilman@users.noreply.github.com>
2024-12-14 11:01:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import cors from 'cors';
 import dotenv from 'dotenv';
 import router from './routes/index.js';
 
@@ -7,18 +6,6 @@ dotenv.config(); // Load konfigurasi dari .env
 
 const app = express();
 const PORT = process.env.PORT || 5000; // Gunakan PORT dari .env jika ada
-
-// Konfigurasi CORS
-app.use(cors({
-  origin: [
-    'http://127.0.0.1:5500',
-    'http://localhost:5500',
-    'http://127.0.0.1:3000',
-    'http://localhost:3000'
-  ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
 
 app.use(express.json());
 app.use(router);
